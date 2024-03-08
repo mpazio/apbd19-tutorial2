3930a027fbfd97f7ad7a93bff60d9c30fecf5498
Michal Pazio <elmiciu@gmail.com>
2024-03-08 18:46:42 +0100

    Add GetAverage method

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+static double GetAverage(int[] numbers)
+{
+    double sum = 0.0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return sum / numbers.Length;
+}
+
